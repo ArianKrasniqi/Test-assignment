25a635794b0e6c07caf7afd26d35f28737552967
ArianKrasniqi <ariiankrasniqi@gmail.com>
2021-08-12 12:05:45 +0200

    Update Cache after Mutation

--- src/views/Company/Add/Add.jsx
+++ src/views/Company/Add/Add.jsx
@@ -2,139 +2,53 @@ import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router";
 import Modal from "components/Modal/Modal.component";
 import Backdrop from "components/Backdrop/Backdrop.component";
-import { sectorsData, stages } from "data/data";
+import Form from "../Form/Form";
 
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-import NativeSelect from "@material-ui/core/NativeSelect";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
-import styled from "styled-components";
-import InputBase from "@material-ui/core/InputBase";
-
-import { ADD_COMPANY } from "../../../Page.queries";
-import { useMutation } from '@apollo/client';
-
-export const AddCompanyForm = styled.div`
-  margin: 20px 100px;
-  width: 500px;
-  color: #267384;
-`;
-
-export const Title = styled.h2`
-  text-align: center;
-  margin-bottom: 40px;
-`;
-
-export const LabeledInput = styled.div`
-  margin-bottom: 20px;
-`;
-
-export const Label = styled.label`
-  color: black;
-  font-size: 14px;
-`;
-
-export const LabelInside = styled.span`
-  position: absolute;
-  right: 20px;
-  top: calc(50% + 4px);
-  transform: translateY(-50%);
-  color: #c0c9d0;
-`;
-
-export const Input = styled.input`
-  border: 2px solid #b9c3cd;
-  width: 100%;
-  height: 45px;
-  border-radius: 6px;
-  margin-top: 8px;
-  padding: 10px 0 10px 12px;
-  font-size: 15px;
-
-  ::placeholder {
-    color: #c0c9d0;
-  }
-`;
-
-export const Submit = styled.button`
-  border: none;
-  width: 200px;
-  border-radius: 4px;
-  height: 50px;
-  color: white;
-  background-color: #0f7380;
-  background-color: ${props => props.allowSubmit ? "#0f7380" : "#a8aaab90"};
-  margin-left: 50%;
-  transform: translateX(-50%);
-  margin-top: 20px;
-  cursor: ${props => props.allowSubmit ? "pointer" : "not-allowed"};
-`;
-
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-  select: {
-    width: "100%",
-    marginBottom: "20px",
-  },
-  inputLabel: {
-    color: "black",
-    fontSize: "19px",
-  },
-}));
-
-const BootstrapInput = withStyles((theme) => ({
-  root: {
-    "label + &": {
-      marginTop: theme.spacing(3),
-    },
-  },
-  input: {
-    borderRadius: 4,
-    position: "relative",
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #B9C3CD",
-    fontSize: 15,
-    padding: "10px 26px 10px 12px",
-    color: "#C0C9D0",
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: ["-apple-system", "BlinkMacSystemFont", "Arial"].join(","),
-    "&:focus": {
-      borderRadius: 4,
-      borderColor: "#80bdff",
-      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
-    },
-  },
-}))(InputBase);
+import { ADD_COMPANY, GET_COMPANIES } from "../../../Page.queries";
+import { useMutation } from "@apollo/client";
 
 const AddCompany = ({ history }) => {
-  const classes = useStyles();
-  const [allowSubmit, setAllowSubmit] = useState(true)
+  const [allowSubmit, setAllowSubmit] = useState(true);
   const [inputs, setInputs] = useState({
-    company: '',
-    stage: 'Idea',
-    sector: 'Fintech',
-    amount: 0
+    company: "",
+    stage: "Idea",
+    sector: "Fintech",
+    amount: 0,
   });
 
-  const [addCompanyFunction, { data, loading, error }] = useMutation(ADD_COMPANY);
-  
+  const [addCompanyFunction, { data: addedData, loading, error }] =
+    useMutation(ADD_COMPANY);
+
+  // useEffect(() => {
+  //   We can send added data in redux and rerender to show in list
+  //   console.log('addedData', addedData)
+  // }, [addedData])
+
   const handleSubmit = () => {
     let data = inputs;
     data.name = inputs.company;
     data.investmentSize = parseInt(inputs.amount || 0);
     delete data.company;
     delete data.amount;
-    addCompanyFunction({ variables: { ...data } })
-    window.location.href = "/"
-  }
-  
+    addCompanyFunction({
+      variables: { ...data },
+      update: (cache) => {
+        const existingCompanies = cache.readQuery({ query: GET_COMPANIES });
+        const newCompanies = [...existingCompanies.companies]
+        newCompanies.push({...data, id: new Date().getUTCMilliseconds()});
+        cache.writeQuery({
+          query: GET_COMPANIES,
+          data: { companies: newCompanies },
+        });
+      },
+    });
+    history.push('/')
+  };
+
   useEffect(() => {
     let filled = true;
     for (const property in inputs) {
-      if(!inputs[property]) filled = false;
+      if (!inputs[property]) filled = false;
     }
     setAllowSubmit(filled);
   }, [inputs]);
@@ -149,85 +63,19 @@ const AddCompany = ({ history }) => {
   const handleInputChange = (e) => {
     setInputs({
       ...inputs,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <Backdrop show={true} clicked={handleClose}>
       <Modal show={true} clicked={(e) => modalClicked(e)}>
-        <AddCompanyForm>
-          <Title>ADD NEW COMPANY</Title>
-
-          <LabeledInput>
-            <Label htmlFor="company">Company Name</Label>
-            <Input
-              type="text"
-              id="company"
-              name="company"
-              placeholder="Company Name"
-              onChange={handleInputChange}
-            />
-          </LabeledInput>
-
-          <FormControl className={classes.select}>
-            <InputLabel
-              className={classes.inputLabel}
-              htmlFor="demo-customized-select-native"
-            >
-              Stage
-            </InputLabel>
-            <NativeSelect
-              id="demo-customized-select-native"
-              value={inputs.stage}
-              name="stage"
-              onChange={handleInputChange}
-              input={<BootstrapInput />}
-            >
-              <option aria-label="None" value="" />
-              {stages.map((stage) => (
-                <option key={stage} value={stage}>{stage}</option>
-              ))}
-            </NativeSelect>
-          </FormControl>
-
-          <FormControl className={classes.select}>
-            <InputLabel
-              className={classes.inputLabel}
-              htmlFor="demo-customized-select-native"
-            >
-              Sector
-            </InputLabel>
-            <NativeSelect
-              id="demo-customized-select-native"
-              value={inputs.sector}
-              name="sector"
-              onChange={handleInputChange}
-              input={<BootstrapInput />}
-            >
-              <option aria-label="None" value="" />
-              {sectorsData.map((sector) => (
-                <option key={sector.type} value={sector.type}>{sector.type}</option>
-              ))}
-            </NativeSelect>
-          </FormControl>
-
-          <LabeledInput>
-            <Label htmlFor="amount">Investment size</Label>
-            <div style={{ position: "relative" }}>
-              <Input
-                type="number"
-                id="amount"
-                name="amount"
-                placeholder="Enter amount"
-                onChange={handleInputChange}
-              />
-              <LabelInside>EUR</LabelInside>
-            </div>
-          </LabeledInput>
-
-          <Submit onClick={handleSubmit} allowSubmit={allowSubmit}>Add new company</Submit>
-        </AddCompanyForm>
+        <Form
+          handleInputChange={(e) => handleInputChange(e)}
+          inputs={inputs}
+          allowSubmit={allowSubmit}
+          handleSubmit={handleSubmit}
+        />
       </Modal>
     </Backdrop>
   );
--- src/views/Company/Form/Form.jsx
+++ src/views/Company/Form/Form.jsx
@@ -1,5 +1,185 @@
-import React from 'react'
+import React from "react";
+import { sectorsData, stages } from "data/data";
 
-const Form = () => <div />
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import styled from "styled-components";
+import InputBase from "@material-ui/core/InputBase";
+
+export const AddCompanyForm = styled.div`
+  margin: 20px 100px;
+  width: 500px;
+  color: #267384;
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+export const LabeledInput = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const Label = styled.label`
+  color: black;
+  font-size: 14px;
+`;
+
+export const LabelInside = styled.span`
+  position: absolute;
+  right: 20px;
+  top: calc(50% + 4px);
+  transform: translateY(-50%);
+  color: #c0c9d0;
+`;
+
+export const Input = styled.input`
+  border: 2px solid #b9c3cd;
+  width: 100%;
+  height: 45px;
+  border-radius: 6px;
+  margin-top: 8px;
+  padding: 10px 0 10px 12px;
+  font-size: 15px;
+
+  ::placeholder {
+    color: #c0c9d0;
+  }
+`;
+
+export const Submit = styled.button`
+  border: none;
+  width: 200px;
+  border-radius: 4px;
+  height: 50px;
+  color: white;
+  background-color: #0f7380;
+  background-color: ${props => props.allowSubmit ? "#0f7380" : "#a8aaab90"};
+  margin-left: 50%;
+  transform: translateX(-50%);
+  margin-top: 20px;
+  cursor: ${props => props.allowSubmit ? "pointer" : "not-allowed"};
+`;
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  select: {
+    width: "100%",
+    marginBottom: "20px",
+  },
+  inputLabel: {
+    color: "black",
+    fontSize: "19px",
+  },
+}));
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    "label + &": {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #B9C3CD",
+    fontSize: 15,
+    padding: "10px 26px 10px 12px",
+    color: "#C0C9D0",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: ["-apple-system", "BlinkMacSystemFont", "Arial"].join(","),
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}))(InputBase);
+
+const Form = ({ handleInputChange, handleSubmit, inputs, allowSubmit }) => {
+  const classes = useStyles();
+
+  return (
+        <AddCompanyForm>
+          <Title>ADD NEW COMPANY</Title>
+
+          <LabeledInput>
+            <Label htmlFor="company">Company Name</Label>
+            <Input
+              type="text"
+              id="company"
+              name="company"
+              placeholder="Company Name"
+              onChange={handleInputChange}
+            />
+          </LabeledInput>
+
+          <FormControl className={classes.select}>
+            <InputLabel
+              className={classes.inputLabel}
+              htmlFor="demo-customized-select-native"
+            >
+              Stage
+            </InputLabel>
+            <NativeSelect
+              id="demo-customized-select-native"
+              value={inputs.stage}
+              name="stage"
+              onChange={handleInputChange}
+              input={<BootstrapInput />}
+            >
+              <option aria-label="None" value="" />
+              {stages.map((stage) => (
+                <option key={stage} value={stage}>{stage}</option>
+              ))}
+            </NativeSelect>
+          </FormControl>
+
+          <FormControl className={classes.select}>
+            <InputLabel
+              className={classes.inputLabel}
+              htmlFor="demo-customized-select-native"
+            >
+              Sector
+            </InputLabel>
+            <NativeSelect
+              id="demo-customized-select-native"
+              value={inputs.sector}
+              name="sector"
+              onChange={handleInputChange}
+              input={<BootstrapInput />}
+            >
+              <option aria-label="None" value="" />
+              {sectorsData.map((sector) => (
+                <option key={sector.type} value={sector.type}>{sector.type}</option>
+              ))}
+            </NativeSelect>
+          </FormControl>
+
+          <LabeledInput>
+            <Label htmlFor="amount">Investment size</Label>
+            <div style={{ position: "relative" }}>
+              <Input
+                type="number"
+                id="amount"
+                name="amount"
+                placeholder="Enter amount"
+                onChange={handleInputChange}
+              />
+              <LabelInside>EUR</LabelInside>
+            </div>
+          </LabeledInput>
+
+          <Submit onClick={handleSubmit} allowSubmit={allowSubmit}>Add new company</Submit>
+        </AddCompanyForm>
+  );
+};
 
 export default Form;
